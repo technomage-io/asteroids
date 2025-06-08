2d3098b03839432dcece9399b5e35ff9d99a731c
technomage-io <tbenish@gmail.com>
2025-06-08 09:02:56 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    print(f"Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True: 
+        pygame.display.flip()
+       
+        for event in pygame.event.get():
+            if event.type -- pygame.QUIT:
+                return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
